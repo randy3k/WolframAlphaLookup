07601c168bf2e8cc18edd5b9627581f3dbe0bb21
Randy Lai <randy.cs.lai@gmail.com>
2016-05-25 01:39:12 -0400

    my personal favour

--- WolframAlphaLookup.py
+++ WolframAlphaLookup.py
@@ -4,21 +4,26 @@ from xml.etree import ElementTree as ET
 
 class WolframAlphaLookupCommand(sublime_plugin.WindowCommand):
     def run(self):
-        settings = sublime.load_settings("Preferences.sublime-settings") 
+        sublime.set_timeout_async(self.run_async, 1)
+
+    def run_async(self):
+        settings = sublime.load_settings("Preferences.sublime-settings")
         if settings.has("wolfram_api_key"):
             API_KEY = settings.get("wolfram_api_key")
+            queries = []
             for region in self.window.active_view().sel():
                 if not region.empty():
                     query = self.window.active_view().substr(region)
                 else:
                     query = self.window.active_view().substr(self.window.active_view().line(region))
-                query = query.strip()
+                queries.append(query.strip())
 
+            if queries:
                 r = requests.get("http://api.wolframalpha.com/v2/query", params={
-                    "input": query,
+                    "input": " ".join(queries),
                     "appid": API_KEY
                 })
-                
+
                 root = ET.fromstring(r.text)
                 if root.get('success') == 'true':
                     items = list()
@@ -26,27 +31,15 @@ class WolframAlphaLookupCommand(sublime_plugin.WindowCommand):
                         title = pod.attrib.get('title')
                         plaintext = pod.find('./subpod/plaintext').text
                         if title and plaintext:
-                            items.append([title, plaintext])
-                    
+                            items.append([plaintext, title])
+
                     def on_select(index):
                         if index > -1:
-                            print(items[index])
-                            print(region)
-                            self.window.active_view().run_command("insert_result", {"data": items[index][1]})
+                            sublime.set_clipboard(items[index][0])
 
                     self.window.show_quick_panel(items, on_select)
                 else:
+                    print(r.text)
                     sublime.error_message("Wolfram|Alpha could not understand your query!")
-                break
         else:
             sublime.error_message("Please add a \"wolfram_api_key\" to the settings!")
-
-class InsertResultCommand(sublime_plugin.TextCommand):
-    def run(self, edit, data):
-        for region in self.view.sel():
-            if region.empty():
-                line = self.view.line(region)
-                self.view.insert(edit, line.end(), '\n' + (data[:-1] if data[-1] == '\n' else data))
-            else:
-                self.view.insert(edit, region.end(), data)
-            break
